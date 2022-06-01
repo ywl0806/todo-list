82b3ff06f7c330c7abbc9b7845c9ff0263d7c97f
unknown <ywl0806@gmail.com>
2022-06-01 19:21:46 +0900

    create task

--- client/src/components/CreateTask.js
+++ client/src/components/CreateTask.js
@@ -1,16 +1,68 @@
+import { useState } from "react";
 import { request } from "../utils/axios";
 
 const CreateTask = () => {
-  const send = () => {
-    request("post", "/task/createTask", {
-      title: "제목",
-      content: "시발",
-      deadLine: "sdfsdf",
-    });
+  const [title, setTitle] = useState("");
+  const handleTitle = (e) => setTitle(e.target.value);
+  const [content, setContent] = useState("");
+  const handleContent = (e) => setContent(e.target.value);
+  const now = new Date(
+    new Date().getTime() - new Date().getTimezoneOffset() * 60 * 1000
+  );
+
+  const [deadLine, setDeadLine] = useState(now.toISOString().slice(0, 16));
+  const handleDeadLine = (e) => setDeadLine(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await request("post", "/task/createTask", {
+        title,
+        content,
+        deadLine,
+      });
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   return (
     <div>
-      <button onClick={send}>전송</button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title </label>
+          <input
+            onChange={handleTitle}
+            id="title"
+            type="title"
+            placeholder="title"
+            value={title}
+          />
+        </div>
+        <div>
+          <label htmlFor="content">content </label>
+          <input
+            onChange={handleContent}
+            id="content"
+            type="text"
+            placeholder="content"
+            value={content}
+          />
+        </div>
+        <div>
+          <label htmlFor="deadLine">Dead Line </label>
+          <input
+            id="deadLine"
+            type="datetime-local"
+            value={deadLine}
+            onChange={handleDeadLine}
+          ></input>
+        </div>
+
+        <button>전송</button>
+      </form>
     </div>
   );
 };
--- server/controller/taskController.js
+++ server/controller/taskController.js
@@ -1,6 +1,20 @@
-export const createTask = (req, res) => {
+import Task from "../model/Task";
+import moment from "moment-timezone";
+
+export const createTask = async (req, res) => {
   const { title, content, deadLine } = req.body;
   const writer = req.session.user;
-  console.log(title, content);
-  console.log(writer);
+
+  try {
+    const newTask = await Task.create({
+      title,
+      content,
+      writer,
+      deadLine,
+    });
+    const task = await Task.find({ deadLine: { $gt: new Date() } });
+    console.log(task);
+  } catch (error) {
+    console.log(error);
+  }
 };
